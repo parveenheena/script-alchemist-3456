b85c95fe58cc1e9350655d79b9049d4fa3c60eb9
Nishant6571 <nishantbhargava.bhargava@gmail.com>
2024-02-23 01:22:21 +0530

    local code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,6 @@ function App() {
   return (
     <Routes>
       <Route path="/dashboard/*" element={<Dashboard />} />
-  
       <Route path="*" element={<Navigate to="/dashboard/home" replace />} />
     </Routes>
   );
